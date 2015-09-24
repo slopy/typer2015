5662b215efa3860e0a913915278242a14045499b
Slopy <michal.marzec@polcode.net>
2015-09-24 09:51:52 +0200

    add AngularJS

---
+++ angular_app/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name typerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the typerApp
+ */
+angular.module('typerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ angular_app/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name typerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the typerApp
+ */
+angular.module('typerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
